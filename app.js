f074862eeb8b1706562f25c5bdec3a9557a58205
YSidorko <yury.sidorko@gmail.com>
2022-03-25 02:05:04 +0100

    - additional test cases for simplex method solution

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // console.log(Number.MAX_VALUE);
 // console.log(Number.MAX_SAFE_INTEGER);
 
-const test = require('./modules/simplex/tests/04');
+const test = require('./modules/simplex/tests/intuit 04.json');
 const { solution } = require('./modules/simplex/simplex');
 console.log(solution(test.input));
-
-
